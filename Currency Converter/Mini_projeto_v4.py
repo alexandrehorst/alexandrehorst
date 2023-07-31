919c773c92966caee52b30c9e6ebf75192af88ed
Alexandre Horstmann <98498152+alexandrehorst@users.noreply.github.com>
2023-07-31 16:01:19 -0300

    Atualização da geração da lista de moedas disponíveis na API
    O código faz um request diretamente para a API para descobrir as moedas disponíveis e cria um dicionário contendo os códigos. Assim, não será mais necessário conter a listagem de códigos de moedas até mesmo porque algumas delas podem estar insdisponíveis.

--- Currency Converter/Mini_projeto_v4.py
+++ Currency Converter/Mini_projeto_v4.py
@@ -9,6 +9,15 @@ from tkinter.filedialog import askopenfilename
 import pandas as pd
 import os
 
+# Cria um dicionário com os códigos das moedas disponíveis do awesome API
+requisicao = requests.get('https://economia.awesomeapi.com.br/json/all')
+requisicao = requisicao.json()
+cod_moedas = {}
+for item in requisicao:
+    for item2 in requisicao[item]:
+        if item2 == 'name':
+            cod_moedas[item] = requisicao[item]['name'].replace('/Real Brasileiro', '')
+
 
 def periodo_cotacao(primeiro_dia, ultimo_dia):
     # Dia inicial e dia final no formato "yyyy/mm/dd"
@@ -267,50 +276,6 @@ data_cotacao.grid(row=2, column=2, padx=10, pady=10, sticky="NSEW")
 # Insere uma caixa de text para amsg de cotação
 mensagem_cotacao = tk.Label(text="", fg='black', bg='#b0e0e6')
 mensagem_cotacao.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="NSEW")  # Adiciona o label à janela
-
-# Cria um dicionário com os códigos do awesome API
-cod_moedas = {'AED': 'Dirham dos Emirados', 'AFN': 'Afghani do Afeganistão', 'ALL': 'Lek Albanês',
-              'AMD': 'Dram Armênio', 'ANG': 'Guilder das Antilhas', 'AOA': 'Kwanza Angolano', 'ARS': 'Peso Argentino',
-              'AUD': 'Dólar Australiano', 'AZN': 'Manat Azeri', 'BAM': 'Marco Conversível', 'BBD': 'Dólar de Barbados',
-              'BDT': 'Taka de Bangladesh', 'BGN': 'Lev Búlgaro', 'BHD': 'Dinar do Bahrein',
-              'BIF': 'Franco Burundinense', 'BND': 'Dólar de Brunei', 'BOB': 'Boliviano', 'BSD': 'Dólar das Bahamas',
-              'BTC': 'Bitcoin', 'BWP': 'Pula de Botswana', 'BYN': 'Rublo Bielorrusso', 'BZD': 'Dólar de Belize',
-              'CAD': 'Dólar Canadense', 'CHF': 'Franco Suíço', 'CLP': 'Peso Chileno', 'CNH': 'Yuan chinês offshore',
-              'CNY': 'Yuan Chinês', 'COP': 'Peso Colombiano', 'CRC': 'Colón Costarriquenho', 'CUP': 'Peso Cubano',
-              'CVE': 'Escudo cabo-verdiano', 'CZK': 'Coroa Checa', 'DJF': 'Franco do Djubouti',
-              'DKK': 'Coroa Dinamarquesa', 'DOGE': 'Dogecoin', 'DOP': 'Peso Dominicano', 'DZD': 'Dinar Argelino',
-              'EGP': 'Libra Egípcia', 'ETB': 'Birr Etíope', 'ETH': 'Ethereum', 'EUR': 'Euro', 'FJD': 'Dólar de Fiji',
-              'GBP': 'Libra Esterlina', 'GEL': 'Lari Georgiano', 'GHS': 'Cedi Ganês', 'GMD': 'Dalasi da Gâmbia',
-              'GNF': 'Franco de Guiné', 'GTQ': 'Quetzal Guatemalteco', 'HKD': 'Dólar de Hong Kong',
-              'HNL': 'Lempira Hondurenha', 'HRK': 'Kuna Croata', 'HTG': 'Gourde Haitiano', 'HUF': 'Florim Húngaro',
-              'IDR': 'Rupia Indonésia', 'ILS': 'Novo Shekel Israelense', 'INR': 'Rúpia Indiana',
-              'IQD': 'Dinar Iraquiano', 'IRR': 'Rial Iraniano', 'ISK': 'Coroa Islandesa', 'JMD': 'Dólar Jamaicano',
-              'JOD': 'Dinar Jordaniano', 'JPY': 'Iene Japonês', 'KES': 'Shilling Queniano', 'KGS': 'Som Quirguistanês',
-              'KHR': 'Riel Cambojano', 'KMF': 'Franco Comorense', 'KRW': 'Won Sul-Coreano', 'KWD': 'Dinar Kuwaitiano',
-              'KYD': 'Dólar das Ilhas Cayman', 'KZT': 'Tengue Cazaquistanês', 'LAK': 'Kip Laosiano',
-              'LBP': 'Libra Libanesa', 'LKR': 'Rúpia de Sri Lanka', 'LSL': 'Loti do Lesoto', 'LTC': 'Litecoin',
-              'LYD': 'Dinar Líbio', 'MAD': 'Dirham Marroquino', 'MDL': 'Leu Moldavo', 'MGA': 'Ariary Madagascarense',
-              'MKD': 'Denar Macedônio', 'MMK': 'Kyat de Mianmar', 'MNT': 'Mongolian Tugrik', 'MOP': 'Pataca de Macau',
-              'MRO': 'Ouguiya Mauritana', 'MUR': 'Rúpia Mauriciana', 'MVR': 'Rufiyaa Maldiva',
-              'MWK': 'Kwacha Malauiana', 'MXN': 'Peso Mexicano', 'MYR': 'Ringgit Malaio',
-              'MZN': 'Metical de Moçambique', 'NAD': 'Dólar Namíbio', 'NGN': 'Naira Nigeriana',
-              'NIO': 'Córdoba Nicaraguense', 'NOK': 'Coroa Norueguesa', 'NPR': 'Rúpia Nepalesa',
-              'NZD': 'Dólar Neozelandês', 'OMR': 'Rial Omanense', 'PAB': 'Balboa Panamenho', 'PEN': 'Sol do Peru',
-              'PGK': 'Kina Papua-Nova Guiné', 'PHP': 'Peso Filipino', 'PKR': 'Rúpia Paquistanesa',
-              'PLN': 'Zlóti Polonês', 'PYG': 'Guarani Paraguaio', 'QAR': 'Rial Catarense', 'RON': 'Leu Romeno',
-              'RSD': 'Dinar Sérvio', 'RUB': 'Rublo Russo', 'RWF': 'Franco Ruandês', 'SAR': 'Riyal Saudita',
-              'SCR': 'Rúpias de Seicheles', 'SDG': 'Libra Sudanesa', 'SDR': 'DSE', 'SEK': 'Coroa Sueca',
-              'SGD': 'Dólar de Cingapura', 'SOS': 'Shilling Somaliano', 'STD': 'Dobra São Tomé/Príncipe',
-              'SVC': 'Colon de El Salvador', 'SYP': 'Libra Síria', 'SZL': 'Lilangeni Suazilandês',
-              'THB': 'Baht Tailandês', 'TJS': 'Somoni do Tajiquistão', 'TND': 'Dinar Tunisiano',
-              'TRY': 'Nova Lira Turca', 'TTD': 'Dólar de Trinidad', 'TWD': 'Dólar Taiuanês',
-              'TZS': 'Shilling Tanzaniano', 'UAH': 'Hryvinia Ucraniana', 'UGX': 'Shilling Ugandês',
-              'USD': 'Dólar Americano', 'USDT': 'Dólar Americano Turismo', 'UYU': 'Peso Uruguaio',
-              'UZS': 'Som Uzbequistanês', 'VEF': 'Bolívar Venezuelano', 'VND': 'Dong Vietnamita',
-              'VUV': 'Vatu de Vanuatu', 'XAF': 'Franco CFA Central', 'XAGG': 'Prata', 'XAU': 'Ouro',
-              'XBR': 'Brent Spot', 'XCD': 'Dólar do Caribe Oriental', 'XOF': 'Franco CFA Ocidental',
-              'XPF': 'Franco CFP', 'YER': 'Riyal Iemenita', 'ZAR': 'Rand Sul-Africano', 'ZMK': 'Kwacha Zambiana',
-              'ZWL': 'Dólar Zimbabuense'}
 
 # Cria uma lista com as chaves do dicionário
 moedas = []
